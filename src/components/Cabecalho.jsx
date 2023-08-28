f75076b01fa799529d1f6012f4100f1a72c08456
nichol6s <nicholas.jake15@gmail.com>
2023-08-28 16:01:06 -0300

    Correção no cabecalho.jsx

--- src/components/Cabecalho.jsx
+++ src/components/Cabecalho.jsx
@@ -2,7 +2,7 @@ export default function Cabecalho() {
     return (
       <>
         <header>
-          <h1>Vite + React / Coded By - PF0670</h1>
+          <h1>Vite + React / Coded By - RM551809</h1>
           <ul>
             <li> <a href="#">Item - 1</a> </li>
             <li> <a href="#">Item - 2</a> </li>
