8a9dc3b8a0328cb6f70c0b80fc3f67638b1afab9
nichol6s <nicholas.jake15@gmail.com>
2023-09-04 12:16:47 -0300

    Criando as coisas pra editar os produtos

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,17 @@ import Cabecalho from "./components/Cabecalho"
 import Section from "./components/Section"
 import Footer from "./components/Footer"
 import { Outlet } from "react-router-dom";
+//import { useState } from "react";
 
 export default function App(){
+
+  //const [produto, setProduto] = useState("PRODUTO")
+
+  // let count = 0;
+  // const novoValor =()=>{
+  //   count++;
+  //   setProduto(produto +" "+ count);
+  // }
 
   return (
     <>
@@ -16,7 +25,8 @@ export default function App(){
         <Cabecalho/>
 
         {/* Vamos criar uma seção com uma div, com 3 <p></p> e estes <p></p> vão ter 3 linhas de lorem, abaixo dos <p></p> uma img. */}
-
+        {/* <p>Valor do useState :{produto}</p>
+        <button onClick={novoValor}>CLICK</button> */}
         <Outlet/>
 
         {/* Vamos criar um rodapé com uma div, uma lista com 3 itens com links para redes sociais e abaixo um <p></p> com o texto e código do símbolo de direitos reservados. */}
--- src/routes/EditarProdutos.jsx
+++ src/routes/EditarProdutos.jsx
@@ -1,15 +1,51 @@
 import { useParams } from "react-router-dom";
+import { ListaProdutos } from "../components/ListaProdutos";
+import { useState } from "react";
 
 export default function EditarProdutos(){
+
+    document.title = "Editar Produtos";
+
     //Recuperando o ID do produto selecionado
     const {id} = useParams();
     
+    const produtoRecuperadoDaListaById = ListaProdutos.filter(item => item.id == id);
+
+    const [produto, setProduto] = useState({
+        id: produtoRecuperadoDaListaById[0].id,
+        nome: produtoRecuperadoDaListaById[0].id,
+        desc: produtoRecuperadoDaListaById[0].id,
+        valor: produtoRecuperadoDaListaById[0].id,
+    })
+
     return(
         <>
         
         <div>
             <h1>Editar Produtos</h1>
-            <p>Produto Selecionado - {id}</p>
+             
+            <div>
+                <form>
+                    <fieldset>
+                        <legend>PRODUTO SELECIONADO</legend>
+                <div>
+                    <label htmlFor="idNome">Nome do Produto:</label>
+                    <input type="text" name="nome" id="idNome" defaultValue={produto.nome}/>
+                </div>
+                <div>
+                    <label htmlFor="idDesc">Descrição do Produto:</label>
+                    <input type="text" name="desc" id="idDesc" defaultValue={produto.desc}/>
+                </div>
+                <div>
+                    <label htmlFor="idValor">Valor do Produto:</label>
+                    <input type="text" name="valor" id="idValor" defaultValue={produto.valor}/>
+                </div>
+                <div>
+                    <button>Editar</button>
+                </div>
+                    </fieldset>
+                </form>
+            </div>
         </div>
         
         </>
