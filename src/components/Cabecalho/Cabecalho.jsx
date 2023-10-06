4f3a2703ce8db29ecf428fe5fce43afd4d5fd622
nichol6s <nicholas.jake15@gmail.com>
2023-10-06 09:50:21 -0300

    Alternando class active com useLocations.

--- src/components/Cabecalho/Cabecalho.jsx
+++ src/components/Cabecalho/Cabecalho.jsx
@@ -1,15 +1,19 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import {BiHomeSmile as H} from "react-icons/bi"
 import "./Cabecalho.scss"
 
 export default function Cabecalho() {
+
+  const rotaAtual = useLocation();
+  
+
     return (
       <>
         <header className="cabecalho">
           <ul>
-            <li> <Link to="/">HOME - <H/> </Link> </li>
-            <li> <Link to="/produtos">PRODUTOS</Link> </li>
-            <li><Link to="/cadastrar/produto">CADASTRAR PRODUTO</Link></li>
+            <li> <Link to="/" className={rotaAtual.pathname === "/" ? "active" : ""}>HOME - <H/> </Link> </li>
+            <li> <Link to="/produtos" className={rotaAtual.pathname === "/produtos" ? "active" : ""}>PRODUTOS</Link> </li>
+            <li><Link to="/cadastrar/produto" className={rotaAtual.pathname === "/cadastrar/produto" ? "active" : ""}>CADASTRAR PRODUTO</Link></li>
           </ul>
         </header>
       </>
